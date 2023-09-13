6e3cd268f0949d50800c285eeaf7fcd5fc9c6f5d
japneet <japneetsaini084@gmail.com>
2023-09-13 13:55:10 +0530

    Started Card View

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/CardViewDataModel.kt
@@ -0,0 +1,7 @@
+package com.example.recyclerview
+
+import android.graphics.drawable.BitmapDrawable
+import android.widget.ImageView
+
+class CardViewDataModel(var img: Int?, var contactName: String?, var contact: String?) {
+}
