0d9399014175b9366e9ae24ce85c7566788927c1
japneet <japneetsaini084@gmail.com>
2023-09-26 13:55:02 +0530

    Finished adding data into DB

--- RecyclerView/app/src/main/java/com/example/recyclerview/UNIT_4/Db_Sql.kt
+++ RecyclerView/app/src/main/java/com/example/recyclerview/UNIT_4/Db_Sql.kt
@@ -2,11 +2,45 @@ package com.example.recyclerview.UNIT_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.recyclerview.R
 
 class Db_Sql : AppCompatActivity() {
+
+    lateinit var edtName: EditText
+    lateinit var edtAge: EditText
+    lateinit var btnAddData: Button
+    lateinit var btnPrintData: Button
+    lateinit var txtNameDb: TextView
+    lateinit var txtAgeDb: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_db_sql)
+
+        edtName = findViewById(R.id.edtName)
+        edtAge = findViewById(R.id.edtAge)
+        btnAddData = findViewById(R.id.btnAddData)
+        btnPrintData = findViewById(R.id.btnPrintData)
+        txtNameDb = findViewById(R.id.txtNameDB)
+        txtAgeDb = findViewById(R.id.txtAgeDB)
+
+        btnAddData.setOnClickListener {
+            val db = Db_Sql_Demo(this, null)
+
+            val name = edtName.text.toString()
+            val age = edtAge.text.toString()
+
+            db.addData(name, age)
+            edtName.setText("")
+            edtAge.setText("")
+            Toast.makeText(this, "Name : $name added to DB", Toast.LENGTH_SHORT).show()
+        }
+
+        btnPrintData.setOnClickListener {
+
+        }
     }
 }
--- RecyclerView/app/src/main/java/com/example/recyclerview/UNIT_4/Db_Sql_Demo.kt
+++ RecyclerView/app/src/main/java/com/example/recyclerview/UNIT_4/Db_Sql_Demo.kt
@@ -1,15 +1,50 @@
 package com.example.recyclerview.UNIT_4
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class Db_Sql_Demo(context: Context, factory: SQLiteDatabase.CursorFactory?): SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+        val query = ("CREATE TABLE " + TABLE_NAME
+                + " ("
+                + ID_COL + " INTEGER PRIMARY KEY, "
+                + NAME_COL + " TEXT,"
+                + AGE_COL + " TEXT"
+                + ")"
+                )
+        db?.execSQL(query)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+       db?.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
+    }
+
+    fun addData(name: String, age: String){
+        val values = ContentValues()
+        values.put(NAME_COL,name)
+        values.put(AGE_COL,age)
+
+        val db = this.readableDatabase
+        db.insert(TABLE_NAME, null,values)
+        db.close()
+    }
+
+    fun getData(): Cursor?{
+        val db = this.readableDatabase
+
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+    }
+    //companion object: all the properties mentioned here are linked to class itself rather than a single instance of the class
+    companion object{
+        private val DATABASE_NAME = "My_DB"
+        private val DATABASE_VERSION = 1
+        val TABLE_NAME = "My_Table1"
+        val ID_COL = "id"
+        val NAME_COL = "name"
+        val AGE_COL = "age"
     }
 }
